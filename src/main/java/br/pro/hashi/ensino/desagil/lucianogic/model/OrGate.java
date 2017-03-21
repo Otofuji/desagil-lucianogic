40debb5e329100d010637821a5e8eaac26e9920f
Otofuji <ericfoa@al.insper.edu.br>
2017-03-21 15:02:27 -0300

    Or pronto

--- src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/OrGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/lucianogic/model/OrGate.java
@@ -5,12 +5,17 @@ public class OrGate extends Gate {
 	private NandGate nandGate1;
 	private NandGate nandGate2;
 	private NandGate nandGate3;
+	
 
 	public OrGate() {
-		super(1);
+		super(2);
 		nandGate1 = new NandGate();
 		nandGate2 = new NandGate();
 		nandGate3 = new NandGate();
+		
+		nandGate3.connect(nandGate1, 0);
+		nandGate3.connect(nandGate2, 1);
+		
 	}
 
 	@Override
@@ -20,14 +25,15 @@ public class OrGate extends Gate {
 
 	@Override
 	protected void doConnect(Emitter emitter, int index) {
-		nandGate1.connect(emitter, 0);
-		nandGate1.connect(emitter, 1);
+		if (index == 0){
+			nandGate1.connect(emitter, 0);
+			nandGate1.connect(emitter, 1);
+		}
+		else{
+			nandGate2.connect(emitter,  0);
+			nandGate2.connect(emitter, 1);
+		}	
 		
-		nandGate2.connect(emitter, 0);
-		nandGate2.connect(emitter, 1);
-		
-		nandGate3.connect(nandGate1, 0);
-		nandGate3.connect(nandGate3, 1);
 		
 	}
 }
